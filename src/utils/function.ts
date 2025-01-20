7c43c9f6bda4a35b73759ea6a0769b82315be68f
ootsirasit <ootsirasit@gmail.com>
2025-01-20 21:53:21 +0700

    🎨 [APP-0033] Fix submit btn should not show if can not submit

--- src/utils/function.ts
+++ src/utils/function.ts
@@ -1,5 +1,6 @@
 import { isAxiosError } from "axios";
-import { API_ERROR_RESPONSE } from "../constants/constants";
+import { ALLOW_PROBLEM_TYPE, API_ERROR_RESPONSE } from "../constants/constants";
+import { Chapter } from "../layout/redux/submitCodeLayoutSlice";
 
 export const resolveApiError = (error: unknown): API_ERROR_RESPONSE => {
   if (!isAxiosError(error) || !error.response) {
@@ -10,4 +11,32 @@ export const resolveApiError = (error: unknown): API_ERROR_RESPONSE => {
     return data as API_ERROR_RESPONSE;
   }
   return error.response?.data;
+};
+
+export const isTimeInRange = (timeStart: string, timeEnd: string): boolean => {
+  const now = new Date();
+  const startTime = new Date(timeStart);
+  const endTime = new Date(timeEnd);
+
+  return now >= startTime && now <= endTime;
+};
+
+export const checkCanSubmit = (chapterData: Chapter) => {
+  if (
+    chapterData.allow_submit_type === ALLOW_PROBLEM_TYPE.always ||
+    chapterData.allow_submit_type === ALLOW_PROBLEM_TYPE.timerPaused
+  ) {
+    return true;
+  } else if (chapterData.allow_submit_type === ALLOW_PROBLEM_TYPE.deny) {
+    return false;
+  } else if (
+    chapterData.allow_submit_type === ALLOW_PROBLEM_TYPE.timmer ||
+    chapterData.allow_submit_type === ALLOW_PROBLEM_TYPE.dateTime
+  ) {
+    return isTimeInRange(
+      chapterData.submit_time_start,
+      chapterData.submit_time_end
+    );
+  }
+  return false;
 };
