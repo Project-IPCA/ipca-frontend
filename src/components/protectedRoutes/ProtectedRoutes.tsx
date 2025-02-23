0c961e789300b6a66071a247108704faab55f133
ootsirasit <ootsirasit@gmail.com>
2025-02-23 20:01:14 +0700

    👔 [APP-0054] Update eventsource send token to realtime

--- src/components/protectedRoutes/ProtectedRoutes.tsx
+++ src/components/protectedRoutes/ProtectedRoutes.tsx
@@ -17,8 +17,9 @@ function ProtectedRoutes() {
 
   useEffect(() => {
     if (data.profile.f_name) {
+      const token = localStorage.getItem("access_token")
       const evtSource = new EventSource(
-        `${VITE_IPCA_RT}/user/connection/${data.profile.user_id}`
+        `${VITE_IPCA_RT}/user/connection/${data.profile.user_id}?token=${token}`
       );
       evtSource.onmessage = (event) => {
         if (event.data) {
@@ -41,8 +42,9 @@ function ProtectedRoutes() {
 
   useEffect(() => {
     if (data.profile.group_info?.group_id) {
+      const token = localStorage.getItem("access_token")
       const evtSource = new EventSource(
-        `${VITE_IPCA_RT}/group-permission/${data.profile.group_info?.group_id}`
+        `${VITE_IPCA_RT}/group-permission/${data.profile.group_info?.group_id}?token=${token}`
       );
       evtSource.onmessage = (event) => {
         if (event.data) {
